3e6d2c753cf693bdd686e48c32296e0b068a271c
Hae-Young Yoon <haeyoung.yoon@riiid.co>
2023-04-09 12:47:19 +0900

    feat: 프로젝트 구조 셋팅

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,14 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          href="https://cdn.jsdelivr.net/gh/moonspam/NanumSquare@1.0/nanumsquare.css"
+          rel="stylesheet"
+          type="text/css"
+
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
---
+++ src/pages/index.page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import type { FC } from 'react'
+
+const Home: FC = () => {
+  return (
+    <main>
+    </main>
+  )
+}
+
+export default Home;
